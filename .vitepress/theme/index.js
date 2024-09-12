99dfbea3ac9258ee90324d1026f30420c0a15829
Christoph Hochstrasser <christoph@hochstrasser.io>
2024-09-12 22:24:45 +0200

    Use timezone plugin

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -6,9 +6,15 @@ import './style.css'
 import dayjs from 'dayjs'
 import 'dayjs/locale/de-at'
 import relativeTime from 'dayjs/plugin/relativeTime.js'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
 
+dayjs.extend(utc)
+dayjs.extend(timezone)
 dayjs.extend(relativeTime)
 dayjs.locale('de-at')
+
+dayjs.tz.setDefault('Europe/Vienna')
 
 /** @type {import('vitepress').Theme} */
 export default {
