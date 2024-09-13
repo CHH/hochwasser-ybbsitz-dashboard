cd7b0f895726d05d802bdf94b2dac19b796f6a81
Christoph Hochstrasser <christoph@hochstrasser.io>
2024-09-13 09:14:14 +0200

    Styling, imprint

--- .vitepress/theme/HomeLayout.vue
+++ .vitepress/theme/HomeLayout.vue
@@ -8,124 +8,129 @@ import { data as history } from '../../data/history.data.js'
 </script>
 
 <template>
-    <div class="space-y-3">
-        <h1>Dashboard</h1>
+    <div class="">
+        <h1 class="mb-3">Dashboard</h1>
 
-        <section
-            class="border border-sky-600 rounded bg-white"
-            v-for="(river) in rivers"
-        >
-            <header class="py-3 px-6 bg-sky-100 rounded-t grid grid-cols-12 items-center">
-                <h2 class="col-span-6">{{ river.name }}</h2>
-                <div class="col-span-6 text-right">
-                    <div>
-                        {{ (+(pegel[river.id].Value?.Value ?? null)).toLocaleString() }} cm
+        <div class="grid grid-cols-1 xl:grid-cols-2 gap-3 xl:gap-6"> 
+            <section
+                class="rounded-xl bg-white shadow-sm"
+                v-for="(river) in rivers"
+            >
+                <header class="py-3 px-6 rounded-t grid grid-cols-12 items-center">
+                    <h2 class="col-span-6">{{ river.name }}</h2>
+                    <div class="col-span-6 text-right">
+                        <div>
+                            {{ (+(pegel[river.id].Value?.Value ?? null)).toLocaleString() }} cm
+                        </div>
+                        <div class="text-sky-600 text-sm">
+                            {{ dayjs(pegel[river.id].Value?.Key).format('DD.MM.YYYY HH:mm') }}
+                        </div>
                     </div>
-                    <div class="text-sky-600 text-sm">
-                        {{ dayjs(pegel[river.id].Value?.Key).format('DD.MM.YYYY HH:mm') }}
-                    </div>
-                </div>
-            </header>
-
-            <div class="py-3 px-6">                
-                <e-chart
-                    class="border-0"
-                    :height="300"
-                    :options="{
-                        xAxis: [
-                            {
-                                type: 'time',
-                                name: 'Zeit',
-                            }
-                        ],
-                        yAxis: {
-                            name: 'Pegel in cm',
-                            min: 0,
-                            max: Math.max(+pegel[river.id].Key.cLinie3, ...history[river.id].map((it) => +it.Value)),
-                        },
-                        tooltip: {
-                        },
-                        toolbox: {
-                            feature: {
-                                dataZoom: {
-                                    yAxisIndex: 'none'
-                                },
-                                restore: {},
-                                saveAsImage: {}
-                            }
-                        },
-                        dataZoom: [
-                            {
-                                type: 'inside',
-                                start: 0,
-                                end: 100
-                            },
-                            {
-                                start: 0,
-                                end: 100
-                            }
-                        ],
-                        series: [
-                            {
-                                name: `Pegel ${river.name}`,
-                                type: 'line',
-                                step: true,
-                                data: history[river.id].map((it) => (
-                                    [+dayjs(it.Key), it.Value]
-                                )),
-                                markLine: {
-                                    data: [
-                                        {
-                                            name: 'Vorwarnstufe',
-                                            yAxis: +pegel[river.id].Key.cLinie1,
-                                            lineStyle: {
-                                                color: 'yellow',
-                                            },
-                                            label: {
-                                                formatter: '{b}'
-                                            },
-                                            symbol: 'none',
-                                        },
-                                        {
-                                            name: 'Hochwasserwarnstufe',
-                                            yAxis: +pegel[river.id].Key.cLinie2,
-                                            lineStyle: {
-                                                color: 'orange',
-                                            },
-                                            label: {
-                                                formatter: '{b}'
-                                            },
-                                            symbol: 'none',
-                                        },
-                                        {
-                                            name: 'Hochwasseralarmstufe',
-                                            yAxis: +pegel[river.id].Key.cLinie3,
-                                            lineStyle: {
-                                                color: 'red',
-                                            },
-                                            label: {
-                                                formatter: '{b}'
-                                            },
-                                            symbol: 'none',
-                                        },
-                                    ]
+                </header>
+                <div class="py-3 px-6">
+                    <e-chart
+                        class="border-0"
+                        :height="300"
+                        :options="{
+                            xAxis: [
+                                {
+                                    type: 'time',
+                                    name: 'Zeit',
                                 }
-                            }
-                        ]
-                    }"
-                />
-
-                <details class="mt-3">
-                    <summary>Rohdaten</summary>
-
-                    <pre class="text-xs"><code>{{ pegel[river.id] }}</code></pre>
-                    <pre class="text-xs"><code>{{ history[river.id] }}</code></pre>
-                </details>
-            </div>
-        </section>
-
-        <footer>
-            Quelle: <a href="https://pegel.feuerwehr-krems.at/">https://pegel.feuerwehr-krems.at/</a>
-        </footer>
+                            ],
+                            yAxis: {
+                                name: 'Pegel in cm',
+                                min: 0,
+                                max: Math.max(+pegel[river.id].Key.cLinie3, ...history[river.id].map((it) => +it.Value)),
+                            },
+                            tooltip: {
+                            },
+                            toolbox: {
+                                feature: {
+                                    dataZoom: {
+                                        yAxisIndex: 'none'
+                                    },
+                                    restore: {},
+                                    saveAsImage: {}
+                                }
+                            },
+                            dataZoom: [
+                                {
+                                    type: 'inside',
+                                    start: 0,
+                                    end: 100
+                                },
+                                {
+                                    start: 0,
+                                    end: 100
+                                }
+                            ],
+                            series: [
+                                {
+                                    name: `Pegel ${river.name}`,
+                                    type: 'line',
+                                    step: true,
+                                    data: history[river.id].map((it) => (
+                                        [+dayjs(it.Key), it.Value]
+                                    )),
+                                    markLine: {
+                                        data: [
+                                            {
+                                                name: 'Vorwarnstufe',
+                                                yAxis: +pegel[river.id].Key.cLinie1,
+                                                lineStyle: {
+                                                    color: 'yellow',
+                                                },
+                                                label: {
+                                                    formatter: '{b}'
+                                                },
+                                                symbol: 'none',
+                                            },
+                                            {
+                                                name: 'Hochwasserwarnstufe',
+                                                yAxis: +pegel[river.id].Key.cLinie2,
+                                                lineStyle: {
+                                                    color: 'orange',
+                                                },
+                                                label: {
+                                                    formatter: '{b}'
+                                                },
+                                                symbol: 'none',
+                                            },
+                                            {
+                                                name: 'Hochwasseralarmstufe',
+                                                yAxis: +pegel[river.id].Key.cLinie3,
+                                                lineStyle: {
+                                                    color: 'red',
+                                                },
+                                                label: {
+                                                    formatter: '{b}'
+                                                },
+                                                symbol: 'none',
+                                            },
+                                        ]
+                                    }
+                                }
+                            ]
+                        }"
+                    />
+                    <details class="mt-3">
+                        <summary class="text-gray-500 text-sm">Rohdaten</summary>
+                        <pre class="text-xs"><code>{{ pegel[river.id] }}</code></pre>
+                        <pre class="text-xs"><code>{{ history[river.id] }}</code></pre>
+                    </details>
+                </div>
+            </section>
+        </div>
     </div>
+
+    <footer class="mt-9">
+        <div>
+            Die Daten für dieses Dashboard werden alle 30 Minuten aktualisiert.
+        </div>
+
+        <div class="mt-3">
+            Quelle & laufende Daten: <a href="https://pegel.feuerwehr-krems.at/">https://pegel.feuerwehr-krems.at/</a>
+        </div>
+    </footer>
 </template>
--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -7,7 +7,7 @@ const { site, frontmatter } = useData()
 </script>
 
 <template>
-  <div class="p-3">
+  <div class="p-3 xl:p-6">
     <header>
       Hochwasser Pegel Ybbsitz
     </header>
@@ -17,5 +17,23 @@ const { site, frontmatter } = useData()
     <div v-else>
       <Content />
     </div>
+    <footer class="mt-3">
+      <div>
+        <b>Impressum: </b>
+      </div>
+      <div>
+        Ing. Christoph Hochstrasser - hochstrasser web apps
+      </div>
+      <div>
+        Haselgraben 23
+      </div>
+      <div>
+        3341 Ybbsitz
+      </div>
+      <div class="space-x-3 flex">
+        <a href="https://hochstrasser.io">Website &rarr;</a>
+        <a href="https://hochstrasser.io/privacy">Datenschutz &rarr;</a>
+      </div>
+    </footer>
   </div>
 </template>
