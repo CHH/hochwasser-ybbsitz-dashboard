af81defa7cb72930912b28144882635a60e1cf7b
Christoph Hochstrasser <christoph@hochstrasser.io>
2024-09-16 16:00:51 +0200

    Show 5 days

--- data/history.data.js
+++ data/history.data.js
@@ -7,10 +7,10 @@ export default {
         const data = {}
 
         for (const river of Object.values(rivers)) {
-            const until = dayjs().add(2, 'hour').format('YYYY-MM-DDTHH:mm:ss')
-            const from = dayjs().add(2, 'hour').subtract(2, 'day').format('YYYY-MM-DDTHH:mm:ss')
+            const to = dayjs().add(2, 'hour').format('YYYY-MM-DDTHH:mm:ss')
+            const from = dayjs().add(2, 'hour').subtract(5, 'day').format('YYYY-MM-DDTHH:mm:ss')
 
-            const res = await fetch(`https://pegel.feuerwehr-krems.at/api/getPegelwerte?pegelid=${river.id}&d1=${from}&d2=${until}&za=15`)
+            const res = await fetch(`https://pegel.feuerwehr-krems.at/api/getPegelwerte?pegelid=${river.id}&d1=${from}&d2=${to}&za=15`)
             const json = await res.json()
 
             data[river.id] = json
