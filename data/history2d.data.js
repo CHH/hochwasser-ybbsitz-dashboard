2209160064e484b4b4191916fcd91e9a44b4b5ea
Christoph Hochstrasser <christoph@hochstrasser.io>
2024-09-16 16:01:56 +0200

    Add more data files

---
+++ data/history2d.data.js
@@ -0,0 +1,21 @@
+import riversData from './rivers.data.js'
+import dayjs from 'dayjs'
+
+export default {
+    async load() {
+        const rivers = riversData.load()
+        const data = {}
+
+        for (const river of Object.values(rivers)) {
+            const to = dayjs().add(2, 'hour').format('YYYY-MM-DDTHH:mm:ss')
+            const from = dayjs().add(2, 'hour').subtract(2, 'day').format('YYYY-MM-DDTHH:mm:ss')
+
+            const res = await fetch(`https://pegel.feuerwehr-krems.at/api/getPegelwerte?pegelid=${river.id}&d1=${from}&d2=${to}&za=15`)
+            const json = await res.json()
+
+            data[river.id] = json
+        }
+
+        return data
+    }
+}
